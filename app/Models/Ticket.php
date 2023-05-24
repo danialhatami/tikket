2bd7e1e76f21bb1a886a28f32aaea4d061123ab5
Danial <danialhatami94@gmail.com>
2023-05-24 17:58:26 +0330

    add ticketMessages model and modify ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'assignee_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function assignee()
+    {
+        return $this->belongsTo(User::class, 'assignee_id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
 }
